56cffc82df501b94a220ea8e426bae1096b6eff7
Evgeniy Yakush <evgeniy.yakush@gmail.com>
2020-03-25 14:29:48 -0700

    Third

--- src/main/java/core/App.java
+++ src/main/java/core/App.java
@@ -2,6 +2,6 @@ package core;
 
 public class App {
 	public static void main(String[] args) {
-		System.out.println("Wooo I've chaged it...");
+		System.out.println("Wooo I've chaged it...ADDED");
 	}
 }
