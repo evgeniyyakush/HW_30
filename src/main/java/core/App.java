ac1463d135d6f95dc2215ce1940cba0946a7e570
Evgeniy Yakush <evgeniy.yakush@gmail.com>
2021-04-05 17:45:13 -0700

    1st TRY

--- src/main/java/core/App.java
+++ src/main/java/core/App.java
@@ -2,6 +2,6 @@ package core;
 
 public class App {
 	public static void main(String[] args) {
-		System.out.println("Wooo I've chaged it...ADDED");
+		System.out.println("It's going to be a Test Runner!");
 	}
 }
